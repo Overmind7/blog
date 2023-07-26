fb80ae630c5c156e1f57676cc903e24c7f07ebc3
Overmind7 <a1036359215@outlook.com>
2023-07-26 18:10:19 +0800

    change editlink into them congfig

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -195,6 +195,29 @@ module.exports= {
 
         },
 
+        // 在线编辑设置
+        repo: 'Overmind7/blog',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        // repoLabel: '查看源码',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        // docsRepo: 'vuejs/vuepress',
+
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'main',
+
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+
+        // 默认为 "Edit this page"
+        editLinkText: '编辑此页面'
+
     }),
 
 
@@ -246,31 +269,5 @@ module.exports= {
 
 
     ],
-
-    // 在线编辑设置
-    repo: 'Overmind7/blog',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    // repoLabel: '查看源码',
-
-    // 以下为可选的编辑链接选项
-
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    // docsRepo: 'vuejs/vuepress',
-
-    // 假如文档不是放在仓库的根目录下：
-    docsDir: 'docs',
-
-    // 假如文档放在一个特定的分支下：
-    docsBranch: 'main',
-
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-
-
-    // 默认为 "Edit this page"
-    editLinkText: '编辑此页面'
-
-
 
 }
